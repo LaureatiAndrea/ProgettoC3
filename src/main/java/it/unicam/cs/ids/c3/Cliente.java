b23074f6e1ab729941ced93513758001bdf94483
LaureatiAndrea <andreafranc.laureati@studenti.unicam.it>
2021-04-29 16:07:52 +0200

    Creata interfaccia cliente
    Creata interfaccia cliente

---
+++ src/main/java/it/unicam/cs/ids/c3/Cliente.java
@@ -0,0 +1,17 @@
+package it.unicam.cs.ids.c3;
+
+/**
+ * Le classi che implementano questa interfaccia hanno la responsabilità di descrivere un cliente del sistema C3
+ *
+ * @author Laureati Andrea Francesco, Gaetani Marco
+ */
+public interface Cliente {
+
+    String getNome();
+    String setNome();
+    String getCognome();
+    String setCognome();
+    String getIndirizzo();
+    String setIndirizzo();
+
+}
